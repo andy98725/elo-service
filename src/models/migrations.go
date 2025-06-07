233f0d4116af9d8ab2326faa2089c8589154e06c
Andy Hudson <andyhudson725@gmail.com>
2025-06-07 15:36:40 -0700

    Remove bad migrations

--- src/models/migrations.go
+++ src/models/migrations.go
@@ -7,17 +7,6 @@ import (
 )
 
 func Migrate() error {
-
-	// Add matchmakingMachineName to existing games
-	if err := server.S.DB.Exec("ALTER TABLE games ADD COLUMN IF NOT EXISTS matchmaking_machine_name VARCHAR(255)").Error; err != nil {
-		return err
-	}
-
-	// TEMP: Update any existing records that might have null values
-	if err := server.S.DB.Exec("UPDATE games SET matchmaking_machine_name = 'battle-bots-server' WHERE matchmaking_machine_name IS NULL").Error; err != nil {
-		return err
-	}
-
 	if err := server.S.DB.AutoMigrate(&User{}, &Game{}); err != nil {
 		return err
 	}
