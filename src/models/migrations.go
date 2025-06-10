1117aa812b13caca81fccafa15815b7e76be5d64
Andy Hudson <andyhudson725@gmail.com>
2025-06-09 20:30:01 -0700

    Try logging

--- src/models/migrations.go
+++ src/models/migrations.go
@@ -1,13 +1,18 @@
 package models
 
 import (
+	"context"
 	"log/slog"
 
 	"github.com/andy98725/elo-service/src/server"
+	"gorm.io/gorm/logger"
 )
 
 func Migrate() error {
-	if err := server.S.DB.AutoMigrate(User{}, Game{}, Match{}, MatchResult{}); err != nil {
+	server.S.DB = server.S.DB.Debug()
+	server.S.DB.Logger.LogMode(logger.Info)
+	server.S.DB.Logger.Info(context.Background(), "Migrating database")
+	if err := server.S.DB.AutoMigrate(&User{}, &Game{}, &Match{}, &MatchResult{}); err != nil {
 		return err
 	}
 
