a6e86e5040352bb528376a5d14a6c07c448ca1fc
Andy Hudson <andyhudson725@gmail.com>
2025-06-09 21:09:44 -0700

    Temp log db url

--- src/models/migrations.go
+++ src/models/migrations.go
@@ -1,19 +1,14 @@
 package models
 
 import (
-	"context"
 	"log/slog"
 
 	"github.com/andy98725/elo-service/src/server"
 	"github.com/go-gormigrate/gormigrate/v2"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 func Migrate() error {
-	server.S.DB = server.S.DB.Debug()
-	server.S.DB.Logger.LogMode(logger.Info)
-	server.S.DB.Logger.Info(context.Background(), "Migrating database")
 	m := gormigrate.New(server.S.DB, gormigrate.DefaultOptions, []*gormigrate.Migration{
 		{
 			ID: "initial",
