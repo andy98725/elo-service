d3a36645fe6b28230ca860081305ae3e3db0f45a
Andy Hudson <andyhudson725@gmail.com>
2025-06-09 20:59:51 -0700

    Try dropping tables?

--- src/models/migrations.go
+++ src/models/migrations.go
@@ -19,7 +19,9 @@ func Migrate() error {
 		{
 			ID: "initial_create_users",
 			Migrate: func(tx *gorm.DB) error {
-				// create the basic schema only
+				if tx.Migrator().HasTable(&User{}) {
+					return tx.Migrator().DropTable(&User{})
+				}
 				return tx.Migrator().CreateTable(&User{})
 			},
 			Rollback: func(tx *gorm.DB) error {
@@ -29,7 +31,9 @@ func Migrate() error {
 		{
 			ID: "initial_create_games",
 			Migrate: func(tx *gorm.DB) error {
-				// create the basic schema only
+				if tx.Migrator().HasTable(&Game{}) {
+					return tx.Migrator().DropTable(&Game{})
+				}
 				return tx.Migrator().CreateTable(&Game{})
 			},
 			Rollback: func(tx *gorm.DB) error {
@@ -39,6 +43,9 @@ func Migrate() error {
 		{
 			ID: "initial_create_matches",
 			Migrate: func(tx *gorm.DB) error {
+				if tx.Migrator().HasTable(&Match{}) {
+					return tx.Migrator().DropTable(&Match{})
+				}
 				return tx.Migrator().CreateTable(&Match{})
 			},
 			Rollback: func(tx *gorm.DB) error {
@@ -48,6 +55,9 @@ func Migrate() error {
 		{
 			ID: "initial_create_match_results",
 			Migrate: func(tx *gorm.DB) error {
+				if tx.Migrator().HasTable(&MatchResult{}) {
+					return tx.Migrator().DropTable(&MatchResult{})
+				}
 				return tx.Migrator().CreateTable(&MatchResult{})
 			},
 			Rollback: func(tx *gorm.DB) error {
