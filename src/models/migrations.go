bf8e8d4c989241c8b8ce31cb456a144933ceb084
Andy Hudson <andyhudson725@gmail.com>
2025-06-09 21:05:41 -0700

    try locking migrations

--- src/models/migrations.go
+++ src/models/migrations.go
@@ -14,54 +14,20 @@ func Migrate() error {
 	server.S.DB = server.S.DB.Debug()
 	server.S.DB.Logger.LogMode(logger.Info)
 	server.S.DB.Logger.Info(context.Background(), "Migrating database")
-
 	m := gormigrate.New(server.S.DB, gormigrate.DefaultOptions, []*gormigrate.Migration{
 		{
-			ID: "initial_create_users",
+			ID: "initial",
 			Migrate: func(tx *gorm.DB) error {
-				if tx.Migrator().HasTable(&User{}) {
-					return tx.Migrator().DropTable(&User{})
-				}
-				return tx.Migrator().CreateTable(&User{})
+				tx.Exec(`SELECT pg_advisory_lock(42)`)
+				err := tx.AutoMigrate(&User{}, &Game{}, &Match{}, &MatchResult{})
+				tx.Exec(`SELECT pg_advisory_unlock(42)`)
+				return err
 			},
 			Rollback: func(tx *gorm.DB) error {
-				return tx.Migrator().DropTable(&User{})
-			},
-		},
-		{
-			ID: "initial_create_games",
-			Migrate: func(tx *gorm.DB) error {
-				if tx.Migrator().HasTable(&Game{}) {
-					return tx.Migrator().DropTable(&Game{})
-				}
-				return tx.Migrator().CreateTable(&Game{})
-			},
-			Rollback: func(tx *gorm.DB) error {
-				return tx.Migrator().DropTable(&Game{})
-			},
-		},
-		{
-			ID: "initial_create_matches",
-			Migrate: func(tx *gorm.DB) error {
-				if tx.Migrator().HasTable(&Match{}) {
-					return tx.Migrator().DropTable(&Match{})
-				}
-				return tx.Migrator().CreateTable(&Match{})
-			},
-			Rollback: func(tx *gorm.DB) error {
-				return tx.Migrator().DropTable(&Match{})
-			},
-		},
-		{
-			ID: "initial_create_match_results",
-			Migrate: func(tx *gorm.DB) error {
-				if tx.Migrator().HasTable(&MatchResult{}) {
-					return tx.Migrator().DropTable(&MatchResult{})
-				}
-				return tx.Migrator().CreateTable(&MatchResult{})
-			},
-			Rollback: func(tx *gorm.DB) error {
-				return tx.Migrator().DropTable(&MatchResult{})
+				tx.Exec(`SELECT pg_advisory_lock(42)`)
+				err := tx.Migrator().DropTable(&User{}, &Game{}, &Match{}, &MatchResult{})
+				tx.Exec(`SELECT pg_advisory_unlock(42)`)
+				return err
 			},
 		},
 	})
